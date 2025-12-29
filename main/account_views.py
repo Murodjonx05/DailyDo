3a8716ab55368d3d41a4218483d1050e22411b55
Angel <ahwbanaz83@gmail.com>
2025-12-30 01:15:41 +0500

    add select-block clase for block selecting all...., added slider(images not added), site_conf updated(added can_signup parametr for user can signup self

--- main/account_views.py
+++ main/account_views.py
@@ -3,7 +3,9 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.shortcuts import redirect
 from django.contrib.auth import logout
+from django.contrib import messages
 from .forms import MyUserCreationForm
+from site_conf.models import SiteConfig
 
 class LoginView(DjangoLoginView):
     template_name = 'registration/login.html'
@@ -18,6 +20,13 @@ class SignUpView(CreateView):
     form_class = MyUserCreationForm
     template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
+
+    def dispatch(self, request, *args, **kwargs):
+        config = SiteConfig.get_current()
+        if not config or not config.can_signup:
+            messages.error(request, 'Регистрация новых пользователей временно отключена администратором.')
+            return redirect('login')
+        return super().dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
